8a3ca5a9338fea1904c562d5185dc93f00b2fb49
nkgrnkgr <beefupme@gmail.com>
2024-09-21 14:26:20 +0900

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,43 @@
+import { Fragment } from "react";
 import { Link } from "../Link";
 
 function Spacer() {
   return <span className="text-gray-400 mx-2">/</span>;
 }
 
+const LINKS = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "01-client-server",
+    label: "01-client-server",
+  },
+  {
+    href: "02-routing",
+    label: "02-routing",
+  },
+  {
+    href: "03-server-actions",
+    label: "03-server-actions",
+  },
+  {
+    href: "04-cache",
+    label: "04-cache",
+  },
+];
+
 export function Header() {
   return (
     <header className="m-6">
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
-        <Link href="01-client-server">01-client-server</Link>
-        <Spacer />
-        <Link href="02-routing">02-routing</Link>
-        <Spacer />
-        <Link href="03-server-actions">03-server-actions</Link>
-        <Spacer />
-        <Link href="04-cache">04-cache</Link>
+        {LINKS.map(({ href, label }, index) => (
+          <Fragment key={href}>
+            <Link href={href}>{label}</Link>
+            {index !== LINKS.length - 1 && <Spacer />}
+          </Fragment>
+        ))}
       </nav>
     </header>
   );
